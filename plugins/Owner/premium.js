f96200d6ef10716c2e844e17ad68c7cd4114e15e
Prakoso Adi Wibowo <harrypotter.botai@gmail.com>
2026-06-21 19:07:57 +0700

    Update premium.js

--- plugins/Owner/premium.js
+++ plugins/Owner/premium.js
@@ -1,11 +1,21 @@
+// ==========================================
+// O k o - Y o u r A s s i s t a n t
+// Created by : Prks.adiw -> Instagram
+// Part of : OkoAi - WhatsApp Bot
+// Github : Prksoo22
+// 
+// Premium WhatsApp Bot! Not for resale.
+// All forms of ownership are owned by the Maker and the Buyer. It should be noted that the Buyer is prohibited from reselling the Goods.
+//             OkoAi - ©Since of 2020
+// ==========================================
+
 import fs from 'fs';
 const dbPath = './database/users.json';
 
 export default async function (sock, m, text, sender) {
     if (!text.startsWith('.addprem')) return;
     
-    // Cek Owner (Ganti dengan nomor owner kamu)
-    if (sender !== '6285168864066@s.whatsapp.net') return sock.sendMessage(sender, { text: '❌ Fitur khusus Owner!' }, { quoted: m });
+    if (sender !== '628123456789@s.whatsapp.net') return sock.sendMessage(sender, { text: '❌ Fitur khusus Owner!' }, { quoted: m });
 
     const args = text.split(' ');
     const target = args[1]?.replace(/[^0-9]/g, '') + '@s.whatsapp.net';
